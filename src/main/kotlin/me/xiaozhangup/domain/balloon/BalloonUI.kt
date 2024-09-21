40c80c37f267ce8ecbb62d753e64c724c63e35b8
Daniel Zhang <xiaozhangshiw@163.com>
2024-09-22 00:12:57 +0800

    更新 TabooLib

--- src/main/kotlin/me/xiaozhangup/domain/balloon/BalloonUI.kt
+++ src/main/kotlin/me/xiaozhangup/domain/balloon/BalloonUI.kt
@@ -7,9 +7,9 @@ import org.bukkit.inventory.ItemStack
 import taboolib.common.platform.function.console
 import taboolib.common.platform.function.submit
 import taboolib.library.xseries.XMaterial
-import taboolib.library.xseries.XSkull
 import taboolib.module.ui.openMenu
 import taboolib.module.ui.type.PageableChest
+import taboolib.platform.util.SkullTexture
 import taboolib.platform.util.buildItem
 import java.util.*
 
@@ -18,7 +18,7 @@ object BalloonUI {
         name = " "
     }
     private val icon = buildItem(Material.PLAYER_HEAD) {
-        skullTexture = XSkull.SkullTexture(
+        skullTexture = SkullTexture(
             "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTA2ZTZkODNjZjdlZDVjZjdiZjBlMDE4ZWNiNjAzOWIwNDZkOGRjNmRiNTU2OTAxNGZjYWIzN2I2MTdmMTM5OSJ9fX0=",
             UUID(0, 0)
         )
@@ -29,7 +29,7 @@ object BalloonUI {
         colored()
     }
     private val back = buildItem(Material.PLAYER_HEAD) {
-        skullTexture = XSkull.SkullTexture(
+        skullTexture = SkullTexture(
             "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTk3ZTRlMjdhMDRhZmE1ZjA2MTA4MjY1YTliZmI3OTc2MzAzOTFjN2YzZDg4MGQyNDRmNjEwYmIxZmYzOTNkOCJ9fX0=",
             UUID(0, 0)
         )
@@ -81,7 +81,7 @@ object BalloonUI {
             elements { balloons.sortedBy { it.level } }
             onGenerate { _, element, _, _ ->
                 buildItem(Material.PLAYER_HEAD) {
-                    skullTexture = XSkull.SkullTexture(
+                    skullTexture = SkullTexture(
                         element.skull,
                         UUID(0, 0)
                     )
